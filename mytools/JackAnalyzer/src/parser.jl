083696e4e31b76c38230d4df984c7c51ce0eeff0
goropikari <goropikari56@gmail.com>
2020-09-19 16:59:05 +0900

    rename vars and support expresion partially

--- mytools/JackAnalyzer/src/parser.jl
+++ mytools/JackAnalyzer/src/parser.jl
@@ -55,98 +55,146 @@ end
 
 struct Class
     name
-    class_var_decs
-    subr_decs
+    vardecs
+    subrdecs
 end
 
 struct ClassVarDec
-    vardec
+    deckw
     type
-    idents
+    varnames
 end
 
-function current_token(parser::Parser)
-    return parser.tokens[parser.pos]
+########
+# utils
+########
+function current(parser::Parser)
+    if parser.pos <= length(parser.tokens)
+        return parser.tokens[parser.pos]
+    else
+        nothing
+    end
 end
 
-function nexttoken(parser::Parser)
-    if (parser.pos + 1 > length(parser.tokens))
+function next(parser::Parser)
+    if parser.pos + 1 < length(parser.tokens)
+        return parser.tokens[parser.pos + 1]
+    end
+end
+
+function advance!(parser::Parser)
+    if (parser.pos > length(parser.tokens))
         return nothing
     end
     parser.pos += 1
 end
 
-function accept(parser::Parser, typ::TokenType)
-    if current_token(parser).enum == typ
-        nexttoken(parser)
+function accept!(parser::Parser, typ::TokenType)
+    if current(parser).enum == typ
+        advance!(parser)
         return true
     end
     return false
 end
 
 function expect(parser::Parser, typ::TokenType)
-    return current_token(parser).enum == typ
+    return current(parser).enum == typ
 end
 
+function istype(parser)
+    return current(parser).enum in (INT, CHAR, BOOLEAN, IDENTIFIER)
+end
+
+##################
+# parse functions
+##################
 function class(parser::Parser)
-    accept(parser, CLASS)
-    name = class_name(parser)
-    accept(parser, LCPAREN)
-    var_decs = class_var_decs(parser)
-    subr_decs = subroutine_decs(parser)
-    accept(parser, RCPAREN)
+    function class_name(parser::Parser)
+        if expect(parser, IDENTIFIER)
+            token = current(parser)
+            advance!(parser)
+            return token
+        end
+    end
 
-    return Class(name, var_decs, subr_decs)
+    accept!(parser, CLASS)
+    name = class_name(parser)
+    accept!(parser, LCPAREN)
+    vardecs = class_var_decs(parser)
+    subrdecs = subroutine_decs(parser)
+    accept!(parser, RCPAREN)
+
+    return Class(name, vardecs, subrdecs)
 end
 
-function class_name(parser::Parser)
-    if expect(parser, IDENTIFIER)
-        token = current_token(parser)
-        nexttoken(parser)
-        return token
+function class_var_decs(parser)
+    vardecs = []
+    while current(parser).enum in (STATIC, FIELD)
+        push!(vardecs, class_var_dec(parser))
+    end
+
+    return vardecs
+end
+function class_var_dec(parser::Parser)
+    if expect(parser, STATIC) || expect(parser, FIELD)
+        deckw = current(parser)
+        advance!(parser)
+        typ = nothing
+        if istype(parser)
+            typ = current(parser)
+            advance!(parser)
+        end
+        vars = []
+        while expect(parser, IDENTIFIER)
+            push!(vars, current(parser))
+            advance!(parser)
+            accept!(parser, COMMA)
+        end
+        accept!(parser, SEMICOLON)
+
+        return ClassVarDec(deckw, typ, vars)
     end
 end
 
 function subroutine_decs(parser::Parser)
     subr_decs = []
-    while current_token(parser).enum in (CONSTRUCTOR, FUNCTION, METHOD)
+    while current(parser).enum in (CONSTRUCTOR, FUNCTION, METHOD)
         push!(subr_decs, subroutine_dec(parser))
     end
     return subr_decs
 end
-
 function subroutine_dec(parser::Parser)
-    function type(parser)
-        token = current_token(parser)
+    function type!(parser)
+        token = current(parser)
         if token.enum == VOID || istype(parser)
-            nexttoken(parser)
+            advance!(parser)
             return token
         end
     end
     function subrname(parser)
-        token = current_token(parser)
+        token = current(parser)
         if expect(parser, IDENTIFIER)
-            nexttoken(parser)
+            advance!(parser)
             return token
         end
     end
-    subr = current_token(parser)
-    nexttoken(parser)
-    typ = type(parser)
+    subr = current(parser)
+    advance!(parser)
+    typ = type!(parser)
     name = subrname(parser)
-    accept(parser, LPAREN)
+    accept!(parser, LPAREN)
     params = parameters(parser)
-    accept(parser, RPAREN)
-    accept(parser, LCPAREN)
+    accept!(parser, RPAREN)
+    accept!(parser, LCPAREN)
     body = subroutinebody(parser)
-    accept(parser, RCPAREN)
+    accept!(parser, RCPAREN)
 
     return SubroutineDec(subr, typ, name, params, body)
 end
 
 struct SubroutineDec
-    subr
-    typ
+    subr # constructor, method, function
+    type
     name
     params
     body
@@ -156,29 +204,19 @@ function parameters(parser::Parser)
     params = []
     while istype(parser)
         push!(params, parameter(parser))
-        accept(parser, COMMA)
+        accept!(parser, COMMA)
     end
 
     return params
 end
 
-function istype(parser)
-    return current_token(parser).enum in (INT, CHAR, BOOLEAN, IDENTIFIER)
-end
 
 function parameter(parser::Parser)
-    function type(parser)
-        if istype(parser)
-            token = current_token(parser)
-            nexttoken(parser)
-            return token
-        end
-    end
-
-    typ = type(parser)
+    typ = current(parser)
+    advance!(parser)
     varname = if expect(parser, IDENTIFIER)
-        token = current_token(parser)
-        nexttoken(parser)
+        token = current(parser)
+        advance!(parser)
         token
     else
         nothing
@@ -197,29 +235,26 @@ struct Parameter
 end
 
 function subroutinebody(parser::Parser)
-    accept(parser, LCPAREN)
     vars = body_var_decs(parser)
-    stmts = staments(parser)
-    accept(parser, RCPAREN)
+    stmts = statements(parser)
 
     return SubroutineBody(vars, stmts)
 end
 
-
 function body_var_decs(parser::Parser)
     decs = []
-    while accept(parser, VAR)
+    while accept!(parser, VAR)
         vars = []
         typ = nothing
-        istype(parser) || return nothing
-        typ = current_token(parser)
-        nexttoken(parser)
+        istype(parser) || return println("hoge")
+        typ = current(parser)
+        advance!(parser)
         while expect(parser, IDENTIFIER)
-            push!(vars, current_token(parser))
-            nexttoken(parser)
-            accept(parser, COMMA)
+            push!(vars, current(parser))
+            advance!(parser)
+            accept!(parser, COMMA)
         end
-        accept(parser, SEMICOLON)
+        accept!(parser, SEMICOLON)
         push!(decs, SubroutineBodyVarDec(typ, vars))
     end
     return decs
@@ -230,46 +265,194 @@ struct SubroutineBodyVarDec
     vars
 end
 
+abstract type Statement end
+
+
 struct SubroutineBody
-    vars::Vector{Union{Nothing, SubroutineBodyVarDec}}
+    vardecs::Vector{Union{Nothing, SubroutineBodyVarDec}}
+    stmts::Vector{Statement}
+end
+
+function isstatement(parser::Parser)
+    return current(parser).enum in (LET, IF, WHILE, DO, RETURN)
+end
+
+function statements(parser::Parser)
+    stmts = []
+    while isstatement(parser)
+        if expect(parser, LET)
+            stmt = let_stmt(parser)
+            push!(stmts, stmt)
+        elseif expect(parser, IF)
+            stmt = if_stmt(parser)
+            push!(stmts, stmt)
+        elseif expect(parser, WHILE)
+            stmt = while_stmt(parser)
+            push!(stmts, stmt)
+        elseif expect(parser, DO)
+            stmt = do_stmt(parser)
+            push!(stmts, stmt)
+        elseif expect(parser, RETURN)
+            stmt = return_stmt(parser)
+            push!(stmts, stmt)
+        end
+    end
+
+    return stmts
+end
+
+function let_stmt(parser)
+    advance!(parser)
+    var = nothing
+    arr_idx = nothing
+    expr = nothing
+    if expect(parser, IDENTIFIER)
+        var = current(parser)
+        advance!(parser)
+        if accept!(parser, LSQPAREN)
+            arr_idx = expression(parser)
+            accept!(parser, RSQPAREN)
+            var = _Array(var, arr_idx)
+        end
+    end
+    accept!(parser, EQ)
+    expr = expression(parser)
+    accept!(parser, SEMICOLON)
+
+    return Let(var, expr)
+end
+
+struct Let <: Statement
+    var
+    expr
+end
+
+struct _Array
+    var
+    idx
+end
+
+function if_stmt(parser)
+    advance!(parser)
+    accept!(parser, LPAREN)
+    cond = expression(parser)
+    accept!(parser, RPAREN)
+    accept!(parser, LCPAREN)
+    then_stmt = statements(parser)
+    accept!(parser, RCPAREN)
+    else_stmt = nothing
+    if accept!(parser, ELSE)
+        accept!(parser, LCPAREN)
+        else_stmt = statements(parser)
+        accept!(parser, RCPAREN)
+    end
+
+    return If(cond, then_stmt, else_stmt)
+end
+
+struct If <: Statement
+    cond
+    then_stmt
+    else_stmt
+end
+
+function while_stmt(parser::Parser)
+    advance!(parser)
+    accept!(parser, LPAREN)
+    cond = expression(parser)
+    accept!(parser, RPAREN)
+    accept!(parser, LCPAREN)
+    stmts = statements(parser)
+    accept!(parser, RCPAREN)
+
+    return While(cond, stmts)
+end
+
+struct While <: Statement
+    cond
     stmts
 end
 
-function staments(parser::Parser)
+function do_stmt(parser::Parser)
+    advance!(parser)
+    call = subroutine_call(parser)
+    return Do(call)
 end
 
-function class_var_decs(parser)
-    vardecs = []
-    while current_token(parser).enum in (STATIC, FIELD)
-        push!(vardecs, class_var_dec(parser))
+function subroutine_call(parser)
+    # TODO
+end
+
+struct Do <: Statement
+    subr
+end
+
+function return_stmt(parser::Parser)
+    advance!(parser)
+    if expect(parser, SEMICOLON)
+        return Return(nothing)
+    else
+        return Return(expression(parser))
     end
-
-    return vardecs
 end
 
-function class_var_dec(parser::Parser)
-    function type(parser::Parser)
-        token = current_token(parser)
-        if token.enum in (INT, CHAR, BOOLEAN, IDENTIFIER)
-            nexttoken(parser)
+struct Return <: Statement
+    expr
+end
+
+function expression(parser::Parser)
+    # TODO: support more term
+    return term(parser)
+end
+
+function isoperator(parser::Parser)
+    return current(parser).enum in (PLUS, MINUS, MUL, DIV, AND, OR, LT, GT, EQ)
+end
+
+struct Expression
+    expr
+end
+
+function term(parser::Parser)
+    token = current(parser)
+    if token.enum in (INT_CONST, STRING_CONST, TRUE, FALSE, NULL, THIS)
+        advance!(parser)
+        return token
+    elseif token.enum == LPAREN
+        accept!(parser, LPAREN)
+        expr = expression(parser)
+        accept!(parser, RPAREN)
+        return expr
+    elseif token.enum == IDENTIFIER
+        nexttoken = next(parser)
+        if nexttoken.enum == LSQPAREN # array
+            advance!(parser)
+            advance!(parser)
+            expr = expression(parser)
+            accept!(parser, RSQPAREN)
+            return _Array(token, expr)
+        elseif nexttoken.enum == PERIOD # external class subroutine
+            advance!(parser)
+            advance!(parser)
+            name = token
+            subr = current(parser)
+            advance!(parser)
+            accept(parser, LPAREN)
+            exprs = expressions(parser)
+            accept(parser, RPAREN)
+            return SubroutineCall(name, subr, exprs)
+        elseif nexttoken.enum == LPAREN # subroutine
+            advance!(parser)
+            advance!(parser)
+            expr = expression(parser)
+            accept!(parser)
+            return expr
+        else # simple variable
+            advance!(parser)
             return token
         end
     end
-
-    if expect(parser, STATIC) || expect(parser, FIELD)
-        vardec = current_token(parser)
-        nexttoken(parser)
-        typ = type(parser)
-        vardecs = []
-        while expect(parser, IDENTIFIER)
-            push!(vardecs, current_token(parser))
-            nexttoken(parser)
-            accept(parser, COMMA)
-        end
-        accept(parser, SEMICOLON)
-
-        return ClassVarDec(vardec, typ, vardecs)
-    end
 end
 
-end # module
+
+end # modul
