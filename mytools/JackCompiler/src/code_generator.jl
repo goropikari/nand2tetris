c68185cb466ae5cce5870b94280ec01b57d3da70
goropikari <goropikari56@gmail.com>
2020-09-22 07:34:21 +0900

    compile identifier

--- mytools/JackCompiler/src/code_generator.jl
+++ mytools/JackCompiler/src/code_generator.jl
@@ -1,8 +1,10 @@
 module CodeGenerators
 
-import ..JackCompiler: Keyword,
-    IntegerConstant,
-    StringConstant
+import ..JackCompiler: IntegerConstant,
+    StringConstant,
+    Keyword,
+    Identifier
+
 import ..Parsers: Parser,
     Program,
     Class,
@@ -45,8 +47,8 @@ end
 
 mutable struct ClassSymbolTable
     symtable::Dict
-    static::Int
-    field::Int
+    staticid::Int
+    fieldid::Int
 
     function ClassSymbolTable()
         return new(Dict(), 0, 0)
@@ -64,13 +66,25 @@ mutable struct SubroutineSymbolTable
 end
 
 function Base.setindex!(x::ClassSymbolTable, val::Variable, key)
-    if !(val.kind in ("static", "field"))
+    if !(val.kind in ("static", "this"))
         error()
     end
     x.symtable[key] = val
 end
 function Base.setindex!(x::SubroutineSymbolTable, val::Variable, key)
     x.symtable[key] = val
+end
+function Base.getindex(x::ClassSymbolTable, key)
+    return x.symtable[key]
+end
+function Base.getindex(x::SubroutineSymbolTable, key)
+    return x.symtable[key]
+end
+function Base.keys(x::ClassSymbolTable)
+    return keys(x.symtable)
+end
+function Base.keys(x::SubroutineSymbolTable)
+    return keys(x.symtable)
 end
 
 
@@ -108,11 +122,14 @@ function cgen(io::IO, codegen::CodeGenerator, subr::SubroutineDec)
     elseif define == "function"
         # only cgen(statements)
     elseif define == "method"
+        codegen.subroutine_symtable["this"] = Variable(codegen.class_name, "argument", 0)
+        _incid!(codegen.subroutine_symtable, "argument")
         print_push_arg(io)
         print_pop_pointer(io)
     else
         error()
     end
+    _register_subroutinevar!(codegen.subroutine_symtable, subr)
     for stmt in subr.body.stmts
         cgen(io, codegen, stmt)
     end
@@ -162,9 +179,28 @@ function cgen(io::IO, codegen::CodeGenerator, keyword::Keyword)
     end
 end
 
+function cgen(io::IO, codegen::CodeGenerator, ident::Identifier)
+    id = ident.val
+    varinfo::Variable = _varinfo(codegen, id)
+    print_push(io, varinfo.kind, varinfo.number)
+end
+
+function cgen(io::IO, codegen::CodeGenerator, arr::_Array)
+    cgen(io, codegen, arr.idx)
+    id = arr.var.val
+    varinfo::Variable = _varinfo(codegen, id)
+    print_push(io, varinfo.kind, varinfo.number)
+    println(io, "add")
+    print_pop_pointer(io, "1")
+    print_push_that(io)
+end
+
 function _register_classvar!(symtb::ClassSymbolTable, class::Class)
     for vardec in class.vardecs
         kind = vardec.deckw.val
+        if kind == "field"
+            kind = "this"
+        end
         type = vardec.type.val
         for var in vardec.varnames
             name = var.val
@@ -175,11 +211,44 @@ function _register_classvar!(symtb::ClassSymbolTable, class::Class)
     end
 end
 
+function _register_subroutinevar!(symtb::SubroutineSymbolTable, subr::SubroutineDec)
+    params = subr.params
+    vardecs = subr.body.vardecs
+    for vardec in vardecs
+        type = vardec.type.val
+        kind = "local"
+        for var in vardec.vars
+            name = var.val
+            num = _getid(symtb, kind)
+            _incid!(symtb, kind)
+            symtb[name] = Variable(type, kind, num)
+        end
+    end
+    for param in params
+        name = param.name.val
+        type = param.type.val
+        kind = "argument"
+        num = _getid(symtb, kind)
+        _incid!(symtb, kind)
+        symtb[name] = Variable(type, kind, num) # type, kind, number
+    end
+    symtb.symtable
+end
+
 function _getid(symtb::ClassSymbolTable, kind)
     if kind == "static"
-        return symtb.static
-    elseif kind == "field"
-        return symtb.field
+        return symtb.staticid
+    elseif kind == "this"
+        return symtb.fieldid
+    else
+        error()
+    end
+end
+function _getid(symtb::SubroutineSymbolTable, kind)
+    if kind == "argument"
+        return symtb.argumentid
+    elseif kind == "local"
+        return symtb.localid
     else
         error()
     end
@@ -187,9 +256,19 @@ end
 
 function _incid!(symtb::ClassSymbolTable, kind)
     if kind == "static"
-        symtb.static += 1
-    elseif kind == "field"
-        symtb.field += 1
+        symtb.staticid += 1
+    elseif kind == "this"
+        symtb.fieldid += 1
+    else
+        error()
+    end
+    nothing
+end
+function _incid!(symtb::SubroutineSymbolTable, kind)
+    if kind == "argument"
+        symtb.argumentid += 1
+    elseif kind == "local"
+        symtb.localid += 1
     else
         error()
     end
@@ -204,9 +283,22 @@ function _count_localvar(subr::SubroutineDec)
     return nlocals
 end
 
+function _varinfo(codegen::CodeGenerator, id)
+    subr_symtb = codegen.subroutine_symtable
+    class_symtb = codegen.class_symtable
+    if id in keys(subr_symtb)
+        return subr_symtb[id]
+    elseif id in keys(class_symtb)
+        return class_symtb[id]
+    else
+        error()
+    end
+end
+
 print_push(io, seg, n="0") = println(io, "push $(seg) $(n)")
 print_push_const(io, n="0") = print_push(io, "constant", n)
 print_push_arg(io, n="0") = print_push(io, "argument", n)
+print_push_that(io, n="0") = print_push(io, "that", n)
 
 print_pop(io, seg, n="0") = println(io, "pop $(seg) $(n)")
 print_pop_pointer(io, n="0") = print_pop(io, "pointer", n)
